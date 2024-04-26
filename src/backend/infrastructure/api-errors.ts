89419344c7661e8810c91b198e3222ed79bf7fd6
carl <carlos.acitores@colba.es>
2024-04-26 21:22:53 +0200

    opensafebox endpoint

--- src/backend/infrastructure/api-errors.ts
+++ src/backend/infrastructure/api-errors.ts
@@ -1,3 +1,17 @@
+export const invalidBasicAuthResponse = Response.json({
+  error: "INVALID_AUTH",
+  message: "Specified Basic Auth does not match"
+}, { 
+  status: 401
+});
+
+export const safeboxNotFoundResponse = Response.json({
+  error: "SAFEBOX_NOT_FOUND",
+  message: "Requested safebox does not exist"
+}, { 
+  status: 404
+});
+
 export const alreadyExistsResponse = Response.json({
   error:   "ALREADY_EXISTS",
   message: "Safebox already exists" 
@@ -12,23 +26,16 @@ export const malformedDataResponse = Response.json({
   status: 422 
 });
 
+export const lockedSafeboxErrorResponse = Response.json({
+  error:   "LOCKED",
+  message: "Safebox is locked, you can not operate" 
+}, { 
+  status: 423 
+});
+
 export const unknownErrorResponse = Response.json({
   error:   "API_ERROR",
   message: "Unexpected API error" 
 }, { 
   status: 500 
-});
-
-export const safeboxNotFoundResponse = Response.json({
-  error: "SAFEBOX_NOT_FOUND",
-  message: "Requested safebox does not exist"
-}, { 
-  status: 404
-});
-
-export const invalidBasicAuthResponse = Response.json({
-  error: "INVALID_AUTH",
-  message: "Specified Basic Auth does not match"
-}, { 
-  status: 404
 });
--- src/backend/open-safebox/api/openSafeboxEndpoint.ts
+++ src/backend/open-safebox/api/openSafeboxEndpoint.ts
@@ -2,11 +2,20 @@ import { getSafeboxFromCosmos } from "@/backend/Safebox";
 import { handler } from "../handler";
 import { NextRequest } from "next/server";
 import { readBasicAuthHeader } from "@/backend/auth/readBasicAuthHeader";
-import { unknownErrorResponse } from "@/backend/infrastructure/api-errors";
+import { invalidBasicAuthResponse, lockedSafeboxErrorResponse, malformedDataResponse, safeboxNotFoundResponse, unknownErrorResponse } from "@/backend/infrastructure/api-errors";
+import { match } from "ts-pattern";
 
 const openSafeBox = handler({
   getSafebox: getSafeboxFromCosmos,
 });
+
+const formatResponse = (result: Awaited<ReturnType<typeof openSafeBox>>) => match(result)
+  .with("invalid-credentials", () => invalidBasicAuthResponse)
+  .with("not-found",           () => safeboxNotFoundResponse)
+  .with("malformed-data",      () => malformedDataResponse)
+  .with("locked",              () => lockedSafeboxErrorResponse)
+  .with({type: "opened"},      ({token}) => Response.json({token}, {status: 200}))
+  .exhaustive();
 
 // TODO - check if this works with Request instead NextRequest
 export const openSafeBoxEndpoint = (request: NextRequest, params: { id: string }) => openSafeBox({safeboxData: {
@@ -18,7 +27,3 @@ export const openSafeBoxEndpoint = (request: NextRequest, params: { id: string }
     console.error(error);
     return unknownErrorResponse;
   });
-
-function formatResponse(result: Awaited<ReturnType<typeof openSafeBox>>) {
-  throw new Error("Function not implemented.");
-}
